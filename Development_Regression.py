3210debe44af42c18ae5a38c1fe63343d396442c
bethli-Bleum <beth.li@bleum.com>
2017-01-03 12:17:20 +0800

    Add files via upload

--- Development_Regression.py
+++ Development_Regression.py
@@ -12,19 +12,22 @@ def data_list(Numpyarray, X):
 def main():
     development_data=pd.read_csv('DevData.csv')
     print("The total number of data:", development_data.size)
-    fig=plt.figure(figsize=(10,10))
+    fig=plt.figure(figsize=(50,50))
     
     #cleaned.shape[1]
     cleaned = np.array(development_data.dropna())
     for i in range(0, 5):
         for j in range(0, 5):
-            print(i, j, '\n')
+            print(i, j, '\n') 
             X=[]
             Y=[]
             data_list(cleaned[:, i], X)
             data_list(cleaned[:, j], Y)
             ax=fig.add_subplot(12, 12, i*12+j+1)
-            ax.scatter(X,Y)
+            if i!=j:
+                ax.scatter(X,Y)
+            #else:
+             #   ax.annotate("Series"+str(i), xycoords='axes fraction', ha="center", va="center")
 
     plt.savefig('relationship.pdf', format='pdf')
 
